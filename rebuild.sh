3ec2896229e90e4fa07420db9dbf928f0f10d161
monkeyidesign <sam@monkeyidesign.com>
2020-06-28 10:38:46 +0700

    Rebuild Database

---
+++ rebuild.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ "$2" == "-db" ]
+
+then
+  echo "rebuilding database..."
+  php bin/console doctrine:schema:drop -n -q --force --full-database
+  # rm src/Migrations/*.php
+  rm migrations/*.php
+
+  php bin/console make:migration
+  php bin/console doctrine:migrations:migrate -n -q
+  php bin/console doctrine:fixtures:load -n -q
+
+fi
+
+if [ -n "$1" ]
